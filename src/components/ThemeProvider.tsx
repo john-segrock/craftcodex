9c9bdaa9b0e9cebda1f7eab87c923ad0295e9181
Junior Developer <johnsegrock@gmail.com>
2025-11-15 04:39:00 +0100

    Revert "Fix Build and Server Issues"

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import type { ComponentProps } from 'react';
-
-type ThemeProviderProps = ComponentProps<typeof NextThemesProvider>;
+import type { ThemeProviderProps } from 'next-themes/dist/types';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
